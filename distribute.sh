406dfa0985e67fd50982e413e16c30ff3168116f
Tomáš Rohlínek <tomas.rohlinek@outlook.cz>
2023-02-17 14:00:25 +0100

    Add distribute script

--- distribute.sh
+++ distribute.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 mkdir -p "$HOME/.config/shell/zsh"
-ln -sf "$PWD/zsh/*" "$HOME/.config/shell/zsh"
+mkdir -p "$HOME/.confim/nvim"
+ln -sf "$PWD/zsh/zinit.zsh" "$HOME/.config/shell/zsh"
+ln -sf "$PWD/zsh/p10k.zsh" "$HOME/.config/shell/zsh"
 ln -sf "$PWD/.zshrc" "$HOME"
 ln -sf "$PWD/.gitconfig" "$HOME"
+ln -sf "$PWD/init.vim" "$HOME/.config/nvim"
