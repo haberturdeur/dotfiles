8d368d781390594f87eb0aa29b3fefafbe4d0d1a
Tomáš Rohlínek <rohlinektomas@gmail.com>
2024-06-27 08:13:02 +0200

    Sync

--- distribute.sh
+++ distribute.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 mkdir -p "$HOME/.config/shell/zsh"
-mkdir -p "$HOME/.confim/nvim"
+mkdir -p "$HOME/.config/nvim"
 ln -sf "$PWD/zsh/zinit.zsh" "$HOME/.config/shell/zsh"
 ln -sf "$PWD/zsh/p10k.zsh" "$HOME/.config/shell/zsh"
 ln -sf "$PWD/.zshrc" "$HOME"
 ln -sf "$PWD/.gitconfig" "$HOME"
 ln -sf "$PWD/init.vim" "$HOME/.config/nvim"
 ln -sf "$PWD/alacritty/" "$HOME/.config"
+ln -sf "$PWD/picom/" "$HOME/.config"
